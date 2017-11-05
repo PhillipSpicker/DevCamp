e7173b8e8fd598c6d93c5785f7a54d5c5ad84a58
Phillip Spicker <phillip.spicker@gmx.de>
2017-11-05 09:48:37 +0000

    added custom scopes to portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+
+end
+
+puts "3 topics created"
+
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -17,7 +27,8 @@
     lomo. Drinking vinegar hot chicken biodiesel cliche cold-pressed kogi 
     hashtag snackwave readymade air plant semiotics. Woke shaman ramps, mixtape 
     bicycle rights PBR&B affogato vinyl sriracha snackwave glossier cred 
-    chartreuse. Subway tile crucifix chartreuse pour-over lyft glossier."
+    chartreuse. Subway tile crucifix chartreuse pour-over lyft glossier.",
+    topic_id: Topic.last.id
   )
   
 end
@@ -33,10 +44,10 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My service",
+    subtitle: "Ruby on Rails",
     body: "Trust fund fanny pack dreamcatcher, blue bottle man braid everyday
     carry tattooed lomo raw denim wolf letterpress 90's YOLO green juice 
     cornhole. Meh edison bulb pok pok, flexitarian tofu woke health goth venmo 
@@ -44,6 +55,19 @@ puts "5 skills created"
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/350x200",
      )
-end     
+end
 
-puts "9 portfolio items created"
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Trust fund fanny pack dreamcatcher, blue bottle man braid everyday
+    carry tattooed lomo raw denim wolf letterpress 90's YOLO green juice 
+    cornhole. Meh edison bulb pok pok, flexitarian tofu woke health goth venmo 
+    irony letterpress butcher keffiyeh affogato.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200",
+     )
+end    
+
+puts "9 portfolio item created"
